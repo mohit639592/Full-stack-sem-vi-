e8ae69888727aae74823454fcbb5be8464933b26
mohit639592 <mohitsharma639592@gmail.com>
2026-02-09 21:33:51 +0530

    9-2-26

--- Date 9-2-26/file.js
+++ Date 9-2-26/file.js
@@ -12,21 +12,76 @@
 
 
 //2nd
-const a = ()=>{
-    b();
-    console.log("a");
+// const a = ()=>{
+//     b();
+//     console.log("a");
+// };
+
+// const b =()=>{
+//     console.log("B");
+//     setTimeout(()=>{
+//         console.log("Timer");
+//     },0);
+// };
+
+// a();
+// console.log("Final");
+
+
+//3rd
+// const a = ()=>{
+//     b();
+//     console.log("a");
+// };
+
+// const b =()=>{
+//     console.log("B");
+//     setTimeout(()=>{
+//         console.log("b Timer1");
+//     },5000);
+//     setTimeout(()=>{
+//         console.log("B timer2");
+//     },3000);
+// };
+
+// a();
+// console.log("Final");
+
+
+
+//4
+// const a = ()=>{
+//     b();
+//     console.log("a");
+// };
+
+// const b =()=>{
+//     console.log("B");
+//     setTimeout(()=>{
+//         console.log("b Timer1");
+//     },0);
+//     setTimeout(()=>{
+//         console.log("B timer2");
+//     },0);
+
+//     Promise.resolve().then (()=>{
+//         console.log("Promise");
+//     })
+// };
+
+// a();
+// console.log("Final");
+
+
+//5
+const sample =(callback)=>{
+    callback("Sample Content","demo");//???????
 };
+const add = (a,b)=>{
+    console.log("data",a+b);
+}
+// sample((data)=>{
+//     console.log("data",data);
+// })
 
-const b =()=>{
-    console.log("B");
-    setTimeout(()=>{
-        console.log("Timer");
-    },0);
-};
-
-a();
-console.log("Final");
-
-
-
-
+sample(add);
